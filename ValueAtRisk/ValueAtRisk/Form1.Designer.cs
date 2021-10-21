ce2ec095a58295bcbe02c01af7e61427e9ffa42a
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-10-21 21:36:14 +0200

    Task 5

--- ValueAtRisk/ValueAtRisk/Form1.Designer.cs
+++ ValueAtRisk/ValueAtRisk/Form1.Designer.cs
@@ -29,13 +29,52 @@ namespace ValueAtRisk
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgw1 = new System.Windows.Forms.DataGridView();
+            this.dgw2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgw1
+            // 
+            this.dgw1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgw1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw1.Location = new System.Drawing.Point(12, 12);
+            this.dgw1.Name = "dgw1";
+            this.dgw1.Size = new System.Drawing.Size(541, 426);
+            this.dgw1.TabIndex = 0;
+            // 
+            // dgw2
+            // 
+            this.dgw2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgw2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgw2.Location = new System.Drawing.Point(559, 12);
+            this.dgw2.Name = "dgw2";
+            this.dgw2.Size = new System.Drawing.Size(229, 426);
+            this.dgw2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgw2);
+            this.Controls.Add(this.dgw1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgw1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgw2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgw1;
+        private System.Windows.Forms.DataGridView dgw2;
     }
 }
 
--- ValueAtRisk/ValueAtRisk/Form1.cs
+++ ValueAtRisk/ValueAtRisk/Form1.cs
@@ -12,9 +12,25 @@ namespace ValueAtRisk
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+        PortfolioEntities context = new PortfolioEntities();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dgw1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        public void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dgw2.DataSource = Portfolio;
         }
     }
 }
