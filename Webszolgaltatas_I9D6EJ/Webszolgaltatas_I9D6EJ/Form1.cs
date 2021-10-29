3e94c9ca00763fe1f2562f6f124dfd4a31f82c17
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-10-29 13:25:47 +0200

    Task 5

--- Webszolgaltatas_I9D6EJ/Webszolgaltatas_I9D6EJ/Form1.cs
+++ Webszolgaltatas_I9D6EJ/Webszolgaltatas_I9D6EJ/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Webszolgaltatas_I9D6EJ.MnbServiceReference;
 using Webszolgaltatas_I9D6EJ.Entities;
+using System.Xml;
 
 namespace Webszolgaltatas_I9D6EJ
 {
@@ -29,9 +30,25 @@ namespace Webszolgaltatas_I9D6EJ
             };
 
             var response = mnbService.GetExchangeRates(request);
-            string result = response.GetExchangeRatesResult;
+            var result = response.GetExchangeRatesResult;
 
             dataGridView1.DataSource = Rates;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement e in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                rate.Date = DateTime.Parse(e.GetAttribute("date"));
+
+                var childElement = (XmlElement)e.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0) rate.Value = value / unit;
+            }
         }
     }
 }
