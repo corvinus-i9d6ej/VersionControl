8446d5dc529ef23e51f199712fab99690bae5473
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-10-29 13:36:51 +0200

    Task 6

--- Webszolgaltatas_I9D6EJ/Webszolgaltatas_I9D6EJ/Form1.cs
+++ Webszolgaltatas_I9D6EJ/Webszolgaltatas_I9D6EJ/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Webszolgaltatas_I9D6EJ.MnbServiceReference;
 using Webszolgaltatas_I9D6EJ.Entities;
 using System.Xml;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Webszolgaltatas_I9D6EJ
 {
@@ -48,6 +49,20 @@ namespace Webszolgaltatas_I9D6EJ
                 var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0) rate.Value = value / unit;
+
+                var series = chartRateData.Series[0];
+                series.ChartType = SeriesChartType.Line;
+                series.XValueMember = "Date";
+                series.YValueMembers = "Value";
+                series.BorderWidth = 2;
+
+                var legend = chartRateData.Legends[0];
+                legend.Enabled = false;
+
+                var chartArea = chartRateData.ChartAreas[0];
+                chartArea.AxisX.MajorGrid.Enabled = false;
+                chartArea.AxisY.MajorGrid.Enabled = false;
+                chartArea.AxisY.IsStartedFromZero = false;
             }
         }
     }
