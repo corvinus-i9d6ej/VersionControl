bf9cdaf0f28d0e04b7b4675d52c23a62298298c9
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-11-05 16:18:34 +0100

    task 3

--- Week7_I9D6EJ/Week7_I9D6EJ/Form1.cs
+++ Week7_I9D6EJ/Week7_I9D6EJ/Form1.cs
@@ -13,9 +13,45 @@ namespace Week7_I9D6EJ
 {
     public partial class Form1 : Form
     {
+        List<Ball> _balls = new List<Ball>();
+        private BallFactory _factory;
+
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+
         public Form1()
         {
             InitializeComponent();
+            Factory = new BallFactory();
+        }
+
+        private void createTimer_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void conveyorTimer_Tick(object sender, EventArgs e)
+        {
+            var position = 0;
+            foreach (var b in _balls)
+            {
+                b.MoveBall();
+                if (b.Left > position)
+                    position = b.Left;
+                if (position == 1000)
+                {
+                    var oldestBall = _balls[0];
+                    mainPanel.Controls.Remove(oldestBall);
+                    _balls.Remove(oldestBall);
+                }
+            }
         }
     }
 }
