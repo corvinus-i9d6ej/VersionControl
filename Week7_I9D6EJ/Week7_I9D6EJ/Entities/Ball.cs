7e5c870d3e94e435b0291eea735fba70a69fef6b
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-11-05 23:43:07 +0100

    task 5

--- Week7_I9D6EJ/Week7_I9D6EJ/Entities/Ball.cs
+++ Week7_I9D6EJ/Week7_I9D6EJ/Entities/Ball.cs
@@ -5,32 +5,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week7_I9D6EJ.Abstractions;
 
 namespace Week7_I9D6EJ.Entities
 {
-    public class Ball: Label
+    public class Ball: Toy
     {
-        public Ball()
-        {
-            AutoSize = false;
-            Height = 50;
-            Width = Height;
-            Paint += Ball_Paint;
-        }
-
-        private void Ball_Paint(object sender, PaintEventArgs e)
-        {
-            DrawImage(e.Graphics);
-        }
-
-        protected void DrawImage(Graphics g)
+        protected override void DrawImage(Graphics g)
         {
             g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
-        }
-
-        public void MoveBall()
-        {
-            Left += 1;
         }
     }
 }
