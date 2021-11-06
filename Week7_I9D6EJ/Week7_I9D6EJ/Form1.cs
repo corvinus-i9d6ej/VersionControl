fc4baf0e97047ef7b41065b43765e83cd14ef414
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-11-06 15:29:42 +0100

    task 8

--- Week7_I9D6EJ/Week7_I9D6EJ/Form1.cs
+++ Week7_I9D6EJ/Week7_I9D6EJ/Form1.cs
@@ -7,51 +7,74 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week7_I9D6EJ.Abstractions;
 using Week7_I9D6EJ.Entities;
 
 namespace Week7_I9D6EJ
 {
     public partial class Form1 : Form
     {
-        List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
-
-        public BallFactory Factory
+        List<Toy> _toys = new List<Toy>();
+        private Toy _nextToy;
+        
+        private IToyFactory _factory;
+        public IToyFactory Factory
         {
             get { return _factory; }
-            set { _factory = value; }
+            set { 
+                _factory = value;
+                DisplayNext();            
+            }
         }
 
+        public void DisplayNext()
+        {
+            if (_nextToy != null) Controls.Remove(_nextToy);
+            _nextToy = Factory.CreateNew();
+            _nextToy.Top = label1.Top + label1.Height + 20;
+            _nextToy.Left = label1.Left;
+            Controls.Add(_nextToy);
+        }
 
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            Factory = new CarFactory();
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add(ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var position = 0;
-            foreach (var b in _balls)
+            foreach (var b in _toys)
             {
                 b.MoveToy();
                 if (b.Left > position)
                     position = b.Left;
                 if (position == 1000)
                 {
-                    var oldestBall = _balls[0];
-                    mainPanel.Controls.Remove(oldestBall);
-                    _balls.Remove(oldestBall);
+                    var oldestToy = _toys[0];
+                    mainPanel.Controls.Remove(oldestToy);
+                    _toys.Remove(oldestToy);
                 }
             }
+        }
+
+        private void btnCar_Click(object sender, EventArgs e)
+        {
+            Factory = new CarFactory();
+        }
+
+        private void btnBall_Click(object sender, EventArgs e)
+        {
+            Factory = new BallFactory();
         }
     }
 }
