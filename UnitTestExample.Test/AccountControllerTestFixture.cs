11d7c537ff2692d0a3b8f201baee0c910925976d
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-11-21 16:36:13 +0100

    Task 3

---
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.Test
+{
+    public class AccountControllerTestFixture
+    {
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            AccountController ac = new AccountController();
+            var result = ac.ValidateEmail(email);
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
