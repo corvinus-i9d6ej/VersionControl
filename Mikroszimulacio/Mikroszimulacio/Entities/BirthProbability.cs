5dbb7790eb5c44571b9aa3e1457131a29e9056c2
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-12-02 18:05:06 +0100

    Task 1 - Entities

---
+++ Mikroszimulacio/Mikroszimulacio/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikroszimulacio.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public byte NumberOfChildren { get; set; }
+        public double Probability { get; set; }
+    }
+}
