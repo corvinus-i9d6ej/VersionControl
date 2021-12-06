0ea7fe11b36f132bdc98af889a76147394de8a44
Zsolt Egyedi <zsolt.egyedi@stud.uni-corvinus.hu>
2021-12-06 20:18:26 +0100

    Task 4

--- EvolutionalAlgorithm/EvolutionalAlgorithm/Form1.cs
+++ EvolutionalAlgorithm/EvolutionalAlgorithm/Form1.cs
@@ -20,6 +20,8 @@ namespace EvolutionalAlgorithm
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+
+        Brain brain;
         public Form1()
         {
             InitializeComponent();
@@ -45,7 +47,35 @@ namespace EvolutionalAlgorithm
             var playerList = from p in gc.GetCurrentPlayers()
                              orderby p.GetFitness() descending
                              select p;
+
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+
+            if (winners.Count() > 0)
+            {
+                brain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+                gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
         }
     }
 }
